fb82cb67a03c09f11f299603a79132d3b985ab32
publish-dt <dvizhenietvorca@gmail.com>
2023-12-27 14:33:47 +0000

    Update from https://github.com/dvizhenietvorca/Signal/commit/da15138b0fd318febd47bd5999605cfae67a533e

---
+++ js/app.js
@@ -0,0 +1,15 @@
+﻿window.PlayAudio = (elementName) => {
+    //debugger;
+    document.getElementById(elementName).play();
+}
+
+window.VolumeChange = (volume) => {
+    //debugger;
+    document.getElementById("pre").volume = volume;
+    document.getElementById("roar").volume = volume;
+}
+
+window.WakeLockMethod = () => {
+    if (navigator.wakeLock)
+        wakeLock = navigator.wakeLock.request('screen');
+}
